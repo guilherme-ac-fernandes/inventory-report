ba32ffc06a492368e35102559063834949886ca7
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-15 19:04:58 -0300

    feat(req 5): construção da classe para leitura de arquivo JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,35 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @staticmethod
     def import_data(path, type):
+        if 'csv' in path:
+            return Inventory.open_csv(path, type)
+        elif 'json' in path:
+            return Inventory.open_json(path, type)
+
+    @staticmethod
+    def open_csv(path, type):
         with open(path, encoding="utf-8") as file:
             inventory = csv.DictReader(file, delimiter=",", quotechar='"')
             if type == 'simples':
                 return SimpleReport.generate(list(inventory))
             elif type == 'completo':
                 return CompleteReport.generate(list(inventory))
+            else:
+                raise ValueError('Type invalid')
+
+    @staticmethod
+    def open_json(path, type):
+        with open(path, mode="r") as file:
+            inventory = json.load(file)
+            if type == 'simples':
+                return SimpleReport.generate(inventory)
+            elif type == 'completo':
+                return CompleteReport.generate(inventory)
             else:
                 raise ValueError('Type invalid')
