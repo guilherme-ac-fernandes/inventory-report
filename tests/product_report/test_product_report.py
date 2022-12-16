1f733a64a4e3743b7bcc49e4a3246a19464939cc
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-15 21:31:34 -0300

    test(req 8): realiza o teste do retorno da função __repr__ da classe Product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product_list = [
+        'Alexa',
+        'Amazon',
+        '12/12/2005',
+        'undefined',
+        'armazenar em local seco',
+    ]
+
+    product = Product(
+        id=1,
+        nome_do_produto=product_list[0],
+        nome_da_empresa=product_list[1],
+        data_de_fabricacao=product_list[2],
+        data_de_validade=product_list[3],
+        numero_de_serie='ALX123456',
+        instrucoes_de_armazenamento=product_list[4],
+    )
+
+    for string in product_list:
+        assert (string in product.__repr__()) is True
